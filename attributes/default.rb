be4077b2b50308de3821b3b5130d7773d5fbe1ea
Cesar Felce <cfelce@gmail.com>
2024-10-27 19:28:51 -0300

    agregar variables en attributos y version nueva de sapcc

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,9 @@
+# SAP
 default["sapjvm"]["version"] = '8.1.092'
+default["sapjvm"]["file"] = 'sapjvm-8.1.101-linux-x64.rpm'
+default['sapcc']['file'] = 'sapcc-2.17.1-linux-x64.zip'
+
+# AWS
+default['aws']['s3'] = 'gtf-sap-repo'
+
+
--- recipes/default.rb
+++ recipes/default.rb
@@ -23,24 +23,24 @@ package 'initscripts' do
 end
 
 # Download sap jvm
-# cookbook_file '/tmp/sapjvm-8.1.092-linux-x64.rpm' do
-#   source 'sapjvm-8.1.092-linux-x64.rpm'
+# cookbook_file '/tmp/sapjvm-8.1.035-linux-x64.rpm' do
+#   source 'sapjvm-8.1.035-linux-x64.rpm'
 #   owner 'root'
 #   group 'root'
 #   mode '0644'
 # end
 
 # Download sap jvm
-remote_file '/tmp/sapjvm-8.1.092-linux-x64.rpm' do
+remote_file "/tmp/#{node['sapjvm']['file']}" do
   owner 'root'
   group 'root'
   mode '0644'
-  source 'https://gtf-sap-repo.s3.amazonaws.com/sapjvm-8.1.092-linux-x64.rpm'
+  source "https://s3.amazonaws.com/#{node['aws']['s3']}/#{node['sapjvm']['file']}"
 end
 
 
 package 'sapjvm' do
-  source '/tmp/sapjvm-8.1.092-linux-x64.rpm'
+  source "/tmp/#{node['sapjvm']['file']}"
   action :install
   # notifies :run, "bash[update-alternatives java]", :immediately
 end
@@ -61,38 +61,29 @@ package "unzip" do
   action :install
 end
 
-remote_file '/tmp/sapcc-2.15.2-linux-x64.zip' do
-  source 'https://gtf-sap-repo.s3.amazonaws.com/sapcc-2.15.2-linux-x64.zip'
+remote_file "/tmp/#{node['sapcc']['file']}" do
+  source "https://s3.amazonaws.com/#{node['aws']['s3']}/#{node['sapcc']['file']}"
   owner 'root'
   group 'root'
   mode '0644'
 end
 
 execute 'unzip-sapcc' do
-  command 'unzip /tmp/sapcc-2.15.2-linux-x64.zip -d /opt/sapcc'
+  command "unzip /tmp/#{node['sapcc']['file']} -d /opt/sapcc"
   action :run
   not_if { ::File.exist?('/opt/sapcc') }
   notifies :install, "package[com.sap.scc-ui]", :immediately
 end
 
 package 'com.sap.scc-ui' do
-  source '/opt/sapcc/com.sap.scc-ui-2.15.2-1.x86_64.rpm'
+  source '/opt/sapcc/com.sap.scc-ui-2.17.1-5.x86_64.rpm'
   action :nothing
   # notifies :run, "bash[update-alternatives java]", :immediately
 end
 
-# bash "install_daemon" do
-# 	action :run
-# 	code <<-EOH
-# 	  update-alternatives --install "/usr/bin/java" "java" "#{jdkFolder}/java" 1
-# 	  update-alternatives --set java #{jdkFolder}/java
-# 	EOH
-# 	not_if { ::File.exist?('/usr/bin/java') }
-# end
-
-# service 'scc_daemon' do
-#   action [:start, :enable]
-# end
+service 'scc_daemon' do
+  action [:start, :enable]
+end
 
 
 
